c880df4d703e02b255cf76090d5b306c760ce1b3
Brent Christensen <brent.a.christensen@gmail.com>
2024-04-23 12:24:01 -0500

    4-23-2024

--- impact_motion/src/wheeltec_gripper_pub_sub.py
+++ impact_motion/src/wheeltec_gripper_pub_sub.py
@@ -13,18 +13,7 @@ class gripper_subscriber(object):
         print("started gripper_subscriber")
         self._cmd_sub = rospy.Subscriber(
             "/wheeltec_gripper_controller/command", JointTrajectory, self.callback,queue_size=10)
-        rospy.spin()
-       
-    def callback(self,ros_msg):
-        self.gripper_state = ros_msg.points[0].positions[0]
-        self._pub = self.gripper_state
-        print (self.gripper_state)
-
-
-
-class gripper_command(object):
-    def __init__(self):
-        # create publisher
+                # create publisher
         self._pub = rospy.Publisher(
             'wheeltec_gripper_position_requested', Float32,queue_size=10)
         # create the msg
@@ -32,12 +21,21 @@ class gripper_command(object):
         # create rate 50 hz
         self._freq = 50
         self._rate = rospy.Rate(self._freq)
-        
+        rospy.spin()
+       
+    def callback(self,ros_msg):
+        self.gripper_state = ros_msg.points[0].positions[0]
+        self._pub.publish(self.gripper_state)
+       
+        print (self.gripper_state)
+
+
+
 
     def run(self):
         while not rospy.is_shutdown():
             # publish message
-            self._pub.publish(self._msg)
+           # self._pub.publish(self._msg)
             self._rate.sleep()
 
 
@@ -47,9 +45,6 @@ if __name__ == '__main__':
     rospy.loginfo("gripper_publisher_node started ...")
 
     # Instantiate object
-    gripper_pub_obj = gripper_command()
     gripper_sub_obj = gripper_subscriber()
-    # run object
-    gripper_pub_obj.run()
-    # spin the node
+
     
